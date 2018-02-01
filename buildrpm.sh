747eb5977d1f95e58f30dbc3da07c105e00ae9fb
Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>
2018-02-01 09:38:39 -0600

    also update rpm/cvmfsutils.spec

--- buildrpm.sh
+++ buildrpm.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-python setup.py bdist_rpm --requires 'python-requests >= 1.1.0,python-dateutil >= 1.4.1'
+RPMREQUIRES='python-requests >= 1.1.0,python-dateutil >= 1.4.1' 
+set -ex
+python setup.py bdist_rpm --requires "$RPMREQUIRES"
+# also update spec file for OBS; check it into git manually
+python setup.py bdist_rpm --requires "$RPMREQUIRES" --spec-only
+mv dist/cvmfsutils.spec rpm
